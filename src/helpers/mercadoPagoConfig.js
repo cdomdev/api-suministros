7e116650e7eff79159d356d54d6e74312f3729ee
cdomdev <dominguez5493.cd@gmail.com>
2024-10-08 15:41:30 -0500

    fix:setting envairoment varibles

--- src/helpers/mercadoPagoConfig.js
+++ src/helpers/mercadoPagoConfig.js
@@ -1,6 +1,6 @@
 import { MercadoPagoConfig } from "mercadopago";
 
-const mercadopagoToken = process.env.VITE_ACCESS_TOKEN_MERCADOPAGO;
+const mercadopagoToken = process.env.ACCESS_TOKEN_MERCADOPAGO;
 
 // Configura las credenciales de Mercado Pago
 export const client = new MercadoPagoConfig({
